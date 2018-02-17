b544cef87a8d8054d64fd8e6a4bb2e9c1038fd06
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-02-17 00:43:24 -0800

    Basic QueryFirms functionality ready

--- src/brokerCheck.js
+++ src/brokerCheck.js
@@ -1,57 +1,69 @@
 createBrokerCheck = () =>{
 
 	const BrokerCheck = {
+
 		endpoint : 'https://doppler.finra.org/doppler-lookup/api/v1/search/',
-		//firmEndpoint : 'https://doppler.finra.org/doppler-lookup/api/v1/search/firms?',
-		//locationEndpoint : 'https://doppler.finra.org/doppler-lookup/api/v1/lookup?',
-		//indivudalEndpoint : 'https://doppler.finra.org/doppler-lookup/api/v1/search/individuals?',
+		createBrokerage : require('./broker.js'),
+		rawParameters : {},
+		translateParameters : (prettyParameters) =>{
 
-		searchType : {
-			firms : 'firms',
-			individuals : 'individuals',
-			lookup : 'lookup'
-		},
-
-		parameters : {
-
-				lat:34.146829,
-				lon:-118.138157,
-				nrows:12,
-				query:'scottrade',
-				r:25,
-				sort:'score+desc',
+			const rawParameters = {
+				query : prettyParameters.query,
+				city : prettyParameters.city,
+				state : prettyParameters.state,
+				nrows : prettyParameters.limit > 0 ? prettyParameters.limit : 10,
+				r : prettyParameters.radius > 0 ? prettyParameters.radius : 25,
+				sort : prettyParameters.sortBy != undefined ? prettyParameters.sortBy : 'score+desc',
+			} 
 			
+			for(key in rawParameters){
+				if(rawParameters[key] === undefined){
+					delete rawParameters[key];
+				}
+			}
+
+			return rawParameters;
+
 		},
 
-		query : (endpoint,searchType,parameters) =>{
-
-			const BrokerCheck = this;
-			const Request = require('request');
-			let queryURL = endpoint + searchType + '?';
+		generateQueryURL : (parameters,queryURL) =>{
 
 			for(key in parameters){
 				queryURL += key + '=' + parameters[key] + '&';
 			}
 
-			
+			return queryURL;
+		},
+		queryFirms : function(parameters, callback) {
+		
+			const rawParameters = this.translateParameters(parameters);
+			const Request = require('request');
+			let queryURL = BrokerCheck.endpoint + 'firms' + '?';
+			queryURL = this.generateQueryURL(rawParameters,queryURL);
 
 			Request(queryURL, (error, response, data) =>{
 
 				if(error){
-					console.log('An error occurred when using the following queryurl:\n' + queryUrl);
+					console.log('An error occurred when using the following queryurl:\n' + queryURL);
 					console.log('Error: ' + error);
 					console.log('Response: ' + response);
 					return;
 				}
 				else{
-					console.log(queryURL);
-					console.log('------------------------------------');
-					console.log(JSON.parse(data).results.BROKER_CHECK_FIRM.results);
+					if(data===null){
+	
+						const rawResultArray = JSON.parse(data).results.BROKER_CHECK_FIRM.results;
+						const parsedBrokers = rawResultArray.map((broker) => { return createBrokerage(broker.fields)});
+						callback(parsedBrokers);
+					}
+
+					else{
+						console.log('No results found');
+					}
+
 				}
 
 			});
-
-
 
 		}
 
@@ -62,11 +74,6 @@ createBrokerCheck = () =>{
 
 }
 
-const BrokerCheck = createBrokerCheck();
-
-BrokerCheck.query(BrokerCheck.endpoint,BrokerCheck.searchType.firms,BrokerCheck.parameters);
-
-
-//module.exports = createBrokerCheck;
+module.exports = createBrokerCheck;
 
 
