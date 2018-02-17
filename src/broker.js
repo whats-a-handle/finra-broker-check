b52533b79df5fdcf8b669f894681969c2dc6535e
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-02-17 00:51:22 -0800

    Broker object initial commit Broker Object is what we parse the results of our query into

---
+++ src/broker.js
@@ -0,0 +1,21 @@
+createBrokerage = (data) => {
+
+	const Brokerage = {
+		secNumber : parseInt(data.bc_sec_number),
+		score : data.score,
+		numberOfBranches : data.bc_branches_count,
+		sourceId : parseInt(data.bc_source_id),
+		name : data.bc_firm_name,
+		otherNames : data.bc_other_names,
+		finraApprovedRegistrationCount : data.bc_approved_finra_registration_count,
+		branchLocations : data.bc_branches,
+	}
+
+	return Brokerage;
+}
+
+
+
+module.exports = createBrokerage;
+
+
