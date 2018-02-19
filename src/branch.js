7d471de67996be88c846e1a44f42883b4f9fb7ea
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2018-02-18 21:17:22 -0800

    Branch Object to parse the inner branch locations Both Firm and Individual queries return Branches that are not easily parsed This object will make it easier to access that data

---
+++ src/branch.js
@@ -0,0 +1,21 @@
+createBranch = (data) =>{
+	
+	const Branch = {
+		address : {
+			zip : data.bc_branch_zip,
+			street : data.bc_branch_street1,
+			state : data.bc_branch_state,
+		},
+		coordinates : {
+			latitude : parseFloat(data.bc_branch_location.split(',')[0]),
+			longitude: parseFloat(data.bc_branch_location.split(',')[1])
+		},
+		branchId : data.bc_branch_id,
+		firmId : data.bc_firm_id != undefined ? data.bc_firm_id : 'n/a',
+	}
+
+	return Branch;
+}
+
+
+module.exports = createBranch;
